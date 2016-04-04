9fc1bcdb550e0cc55f661989fde6858072becc79
Andreas Bergh <andreas__bergh@hotmail.com>
2016-04-04 21:25:13 +0200

    FIxed Jsonreader. Sql import yet not fixed

---
+++ AfterSki/src/AfterSki/Models/RideStatistics.cs
@@ -0,0 +1,161 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AfterSki.Models
+{
+    public class RideStatistic
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime swipeTime { get; set; }
+        public string liftName { get; set; }
+        public int height { get; set; }
+        public string swipeDate { get; set; }
+        public List<RideStatistic> rideStatistics { get; set; }
+        public List<VisitedDestination> visitedDestinations { get; set; }
+        public List<NotVisitedDestination> notVisitedDestinations { get; set; }
+        public List<ActiveSeason> activeSeasons { get; set; }
+        public DefaultSeason defaultSeason { get; set; }
+
+        public class Destination2
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Destination3
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class UsedLift
+        {
+            public string liftName { get; set; }
+            public int liftHeight { get; set; }
+            public int skierRideCount { get; set; }
+            public int skierTotalDropHeight { get; set; }
+            public Destination3 destination { get; set; }
+        }
+
+        public class Destination4
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class NotUsedLift
+        {
+            public string liftName { get; set; }
+            public int liftHeight { get; set; }
+            public int skierRideCount { get; set; }
+            public int skierTotalDropHeight { get; set; }
+            public Destination4 destination { get; set; }
+        }
+
+        public class VisitedDestination
+        {
+            public object season { get; set; }
+            public Destination2 destination { get; set; }
+            public int liftCount { get; set; }
+            public int skierTotalDropHeight { get; set; }
+            public int skierTotalRideCount { get; set; }
+            public int skierTotalLiftCount { get; set; }
+            public bool showNotUsed { get; set; }
+            public string lastVisited { get; set; }
+            public List<UsedLift> usedLifts { get; set; }
+            public List<NotUsedLift> notUsedLifts { get; set; }
+        }
+
+        public class Destination5
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Destination6
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class NotUsedLift2
+        {
+            public string liftName { get; set; }
+            public int liftHeight { get; set; }
+            public int skierRideCount { get; set; }
+            public int skierTotalDropHeight { get; set; }
+            public Destination6 destination { get; set; }
+        }
+
+        public class NotVisitedDestination
+        {
+            public object season { get; set; }
+            public Destination5 destination { get; set; }
+            public int liftCount { get; set; }
+            public int skierTotalDropHeight { get; set; }
+            public int skierTotalRideCount { get; set; }
+            public int skierTotalLiftCount { get; set; }
+            public bool showNotUsed { get; set; }
+            public string lastVisited { get; set; }
+            public List<object> usedLifts { get; set; }
+            public List<NotUsedLift2> notUsedLifts { get; set; }
+        }
+
+        public class ActiveSeason
+        {
+            public int seasonId { get; set; }
+            public string name { get; set; }
+            public int seasonType { get; set; }
+            public string seasonTypeName { get; set; }
+            public string startDate { get; set; }
+            public string endDate { get; set; }
+            public bool isCurrentSeason { get; set; }
+            public string leaderboardString { get; set; }
+            public object months { get; set; }
+            public object weeks { get; set; }
+        }
+
+        public class DefaultSeason
+        {
+            public int seasonId { get; set; }
+            public string name { get; set; }
+            public int seasonType { get; set; }
+            public string seasonTypeName { get; set; }
+            public string startDate { get; set; }
+            public string endDate { get; set; }
+            public bool isCurrentSeason { get; set; }
+            public string leaderboardString { get; set; }
+            public object months { get; set; }
+            public object weeks { get; set; }
+        }
+
+
+        private static T jsonSerializer<T>(string url) where T : new()
+        {
+
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
+
+        public void getSkiData()
+        {
+            string url = "https://www.skistar.com/myskistar/api/v2/views/statisticspage.json?entityId=3206&seasonId=9";
+            var jsData = jsonSerializer<RideStatistic>(url);
+        }
+    }
+}
