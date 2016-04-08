32c35ace78e82b73aac0082b51f2cd4e1f1946b7
andreas lambrant <andreas.lambrant@live.se>
2016-04-08 12:41:44 +0200

    Started on ridePrognosis user story

---
+++ AfterSki/src/AfterSki/Models/RidePrognosis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AfterSki.Models.RideModels;
+
+namespace AfterSki.Models
+{
+    public class RidePrognosis
+    {
+        RideStatisticDBContext rideStats = new RideStatisticDBContext();
+        List<RideStatistic> temp = new List<RideStatistic>();
+
+        public RidePrognosis()
+        {
+            temp = rideStats.RideStatistic.ToList();
+
+        }
+
+    }
+}
