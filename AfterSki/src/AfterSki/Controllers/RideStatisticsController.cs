1245be4a835cddf57f4689bad208d07881924d66
Andreas Bergh <andreas__bergh@hotmail.com>
2016-04-08 12:09:37 +0200

    Added a listfunction to list all ridedata.

---
+++ AfterSki/src/AfterSki/Controllers/RideStatisticsController.cs
@@ -0,0 +1,120 @@
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using AfterSki.Models.RideModels;
+
+namespace AfterSki.Controllers
+{
+    public class RideStatisticsController : Controller
+    {
+        private RideStatisticDBContext _context;
+
+        public RideStatisticsController(RideStatisticDBContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: RideStatistics
+        public IActionResult Index()
+        {
+            return View(_context.RideStatistic.ToList());
+        }
+
+        // GET: RideStatistics/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            RideStatistic rideStatistic = _context.RideStatistic.Single(m => m.id == id);
+            if (rideStatistic == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(rideStatistic);
+        }
+
+        // GET: RideStatistics/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RideStatistics/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(RideStatistic rideStatistic)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.RideStatistic.Add(rideStatistic);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rideStatistic);
+        }
+
+        // GET: RideStatistics/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            RideStatistic rideStatistic = _context.RideStatistic.Single(m => m.id == id);
+            if (rideStatistic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rideStatistic);
+        }
+
+        // POST: RideStatistics/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(RideStatistic rideStatistic)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(rideStatistic);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rideStatistic);
+        }
+
+        // GET: RideStatistics/Delete/5
+        [ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            RideStatistic rideStatistic = _context.RideStatistic.Single(m => m.id == id);
+            if (rideStatistic == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(rideStatistic);
+        }
+
+        // POST: RideStatistics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            RideStatistic rideStatistic = _context.RideStatistic.Single(m => m.id == id);
+            _context.RideStatistic.Remove(rideStatistic);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
