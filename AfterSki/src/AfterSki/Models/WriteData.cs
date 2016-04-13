916e24a1924fb2e72fdebd554a63901912d6863d
Wigge <wigge_94@live.se>
2016-04-13 11:09:35 +0200

    Commit wigge

--- AfterSki/src/AfterSki/Models/WriteData.cs
+++ AfterSki/src/AfterSki/Models/WriteData.cs
@@ -11,6 +11,7 @@ namespace AfterSki.Models
         public class Javaobject
         {
             public int y { get; set; }
+            public string label { get; set; }
         }
 
         public static IEnumerable<Javaobject> PopulateRidesPerDayArray(string rideDate)
@@ -19,11 +20,11 @@ namespace AfterSki.Models
             RideStatisticDBContext rsdb = new RideStatisticDBContext();
             rs = rsdb.RideStatistic.Where(u => u.swipeDate.Contains(rideDate)).ToList();
 
-            var swipeDateArray = rs.Select(x => x.swipeTime).GroupBy(x => x.Hour).Select(group =>
-            new Javaobject{ y = group.Count() })
+            var swipeDateArray = rs.Select(x => x.swipeTime).GroupBy(x => x.Hour).OrderBy( x => x.Key).Select(group =>
+            new Javaobject{ y = group.Count(),label = group.Key.ToString() })
             .ToArray();
         
-
+        
             return swipeDateArray;
 
         }
